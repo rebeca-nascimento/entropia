6328cc8404d3ae0f93d6d79c0b1782b601e8f77e
Rebeca Nascimento Oliveira <133053697+rebeca-nascimento@users.noreply.github.com>
2023-05-30 19:31:48 -0300

    Add files via upload

---
+++ entropia_dados_qualitativos.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 19:24:12 2023
+
+@author: Eraldo
+"""
+
+import numpy as np
+import pandas as pd
+import math
+
+dados = pd.read_csv('D:\Downlods\iris.csv', delimiter=',')
+coluna_qualitativa = dados['class'] 
+frequencia = coluna_qualitativa.value_counts()
+proporcao = frequencia / len(coluna_qualitativa)
+entropia = -sum(proporcao * np.log2(proporcao))
+print('Entropia: ', entropia)
---
+++ entropiamax_dados_qualitativos.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 16:37:43 2023
+
+@author: Eraldo
+"""
+
+import math
+
+def calcular_entropia_maxima(base_dados):
+    total_registros = len(base_dados)
+    valores_unicos = set(base_dados)
+    num_valores_unicos = len(valores_unicos)
+
+    probabilidade = 1 / num_valores_unicos
+
+    entropia_maxima = -1 * sum(probabilidade * math.log2(probabilidade) for n in range(num_valores_unicos))
+
+    return entropia_maxima
+
+
+dados = 'D:\Downlods\iris.csv'
+entropia_max = calcular_entropia_maxima(dados)
+print(f"A entropia máxima da base de dados é: {entropia_max}")
